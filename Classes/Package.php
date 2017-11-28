69a2b6e62b1debb815880e86b911c1791ac68230
Karsten Dambekalns <karsten@dambekalns.de>
2017-11-28 10:57:50 +0100

    BUGFIX: Block assigning duplicate URIs
    If the same URI path is assigned to multiple nodes, the value will be
    suffixed with a counter, to avoid blocking access to "later" documents
    having the same URI.

---
+++ Classes/Package.php
@@ -0,0 +1,53 @@
+<?php
+namespace Flownative\Neos\CustomDocumentUriRouting;
+
+/**
+ * This file is part of the Flownative.Neos.CustomDocumentUriRouting package.
+ *
+ * (c) 2017 Karsten Dambekalns, Flownative GmbH
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Neos\ContentRepository\Domain\Model\Node;
+use Neos\ContentRepository\Domain\Model\NodeInterface;
+use Neos\Eel\FlowQuery\FlowQuery;
+use Neos\Flow\Configuration\ConfigurationManager;
+use Neos\Flow\Core\Bootstrap;
+use Neos\Flow\Package\Package as BasePackage;
+use Neos\Neos\Routing\Cache\RouteCacheFlusher;
+
+/**
+ * The Package class, wiring signal/slot during boot.
+ */
+class Package extends BasePackage
+{
+    /**
+     * @param Bootstrap $bootstrap The current bootstrap
+     * @return void
+     */
+    public function boot(Bootstrap $bootstrap)
+    {
+        $dispatcher = $bootstrap->getSignalSlotDispatcher();
+
+        $dispatcher->connect(Node::class, 'nodePropertyChanged', function (NodeInterface $node, $propertyName) use ($bootstrap) {
+            /** @var string $uriPathPropertyName */
+            $uriPathPropertyName = $bootstrap->getObjectManager()->get(ConfigurationManager::class)->getConfiguration(ConfigurationManager::CONFIGURATION_TYPE_SETTINGS, 'Flownative.Neos.CustomDocumentUriRouting.uriPathPropertyName');
+
+            if ($propertyName === $uriPathPropertyName && !empty($node->getProperty($propertyName)) && $node->getNodeType()->isOfType('Neos.Neos:Document')) {
+                $q = new FlowQuery([$node->getContext()->getCurrentSiteNode()]);
+                $q = $q->context(['invisibleContentShown' => true, 'removedContentShown' => true, 'inaccessibleContentShown' => true]);
+
+                $possibleUriPath = $initialUriPath = $node->getProperty($propertyName);
+                $i = 1;
+                while ($q->find(sprintf('[instanceof Neos.Neos:Document][%s="%s"]', $propertyName, $possibleUriPath))->count() > 0) {
+                    $possibleUriPath = $initialUriPath . '-' . $i++;
+                }
+                $node->setProperty($propertyName, $possibleUriPath);
+            }
+            $bootstrap->getObjectManager()->get(RouteCacheFlusher::class)->registerNodeChange($node);
+        });
+    }
+}
